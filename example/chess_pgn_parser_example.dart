d502bf1427584a9f5835daf5e9c2918cf41b76e7
Bence Hornák <bence.hornak@gmail.com>
2023-03-01 22:09:12 +0100

    Initialize dart package

---
+++ example/chess_pgn_parser_example.dart
@@ -0,0 +1,6 @@
+import 'package:chess_pgn_parser/chess_pgn_parser.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/chess_pgn_parser.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library chess_pgn_parser;
+
+export 'src/chess_pgn_parser_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
