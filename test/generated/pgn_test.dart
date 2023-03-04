7b3a16c2aea0fb3717aeff16d67f0ed9705ea09a
Bence Hornák <bence.hornak@gmail.com>
2023-03-04 16:54:59 +0100

    Add pgn_test.dart

---
+++ test/generated/pgn_test.dart
@@ -0,0 +1,39 @@
+import 'package:antlr4/antlr4.dart';
+import 'package:chess_pgn_parser/src/generated/PGNParser.dart';
+import 'package:chess_pgn_parser/src/generated/PGNLexer.dart';
+import 'package:test/test.dart';
+
+class TreeShapeListener implements ParseTreeListener {
+  @override
+  void enterEveryRule(ParserRuleContext ctx) {
+    print(ctx.text);
+  }
+
+  @override
+  void exitEveryRule(ParserRuleContext node) {}
+
+  @override
+  void visitErrorNode(ErrorNode node) {}
+
+  @override
+  void visitTerminal(TerminalNode node) {}
+}
+
+Future<void> main() async {
+  test('PGNLexer.checkVersion()', () {
+    PGNLexer.checkVersion();
+  });
+  test('PGNParser.checkVersion()', () {
+    PGNParser.checkVersion();
+  });
+  test('Parse simple.pgn', () async {
+    final input = await InputStream.fromPath('test/resources/pgn/simple.pgn');
+    final lexer = PGNLexer(input);
+    final tokens = CommonTokenStream(lexer);
+    final parser = PGNParser(tokens);
+    parser.addErrorListener(DiagnosticErrorListener());
+    parser.buildParseTree = true;
+    final tree = parser.pgn_database();
+    ParseTreeWalker.DEFAULT.walk(TreeShapeListener(), tree);
+  });
+}
