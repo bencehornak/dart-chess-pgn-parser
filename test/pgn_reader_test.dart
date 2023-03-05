d26a972ed3bb6a5c390af1d8788131b6a0801434
Bence Hornák <bence.hornak@gmail.com>
2023-03-06 00:33:54 +0100

    Add test for PgnReader

---
+++ test/pgn_reader_test.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:chess/chess.dart';
+import 'package:chess_pgn_parser/chess_pgn_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final expectedGameTree = GameWithVariations(
+      // Depth: 1st half move
+      [
+        // d4
+        GameNode(
+            AnnotatedMove(Color.WHITE, Chess.SQUARES['d2'], Chess.SQUARES['d4'],
+                0, PieceType.PAWN, null, null),
+            []),
+        // e4
+        GameNode(
+            AnnotatedMove(Color.WHITE, Chess.SQUARES['e2'], Chess.SQUARES['e4'],
+                0, PieceType.PAWN, null, null),
+            // Depth: 2nd half move
+            [
+              // e5
+              GameNode(
+                  AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
+                      Chess.SQUARES['e5'], 0, PieceType.PAWN, null, null),
+                  []),
+              // e6
+              GameNode(
+                  AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
+                      Chess.SQUARES['e6'], 0, PieceType.PAWN, null, null),
+                  [])
+            ])
+      ]);
+  group('simple', () {
+    test('fromString().parse() returns the tree', () async {
+      String input = await File('test/resources/pgn/simple.pgn').readAsString();
+      final reader = PgnReader.fromString(input);
+
+      final tree = reader.parse();
+
+      expect(tree, expectedGameTree);
+    });
+  });
+}
