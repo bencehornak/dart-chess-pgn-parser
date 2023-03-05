a4489c98aa68011fc9ad4b21ff49ce5c478763a7
Bence Hornák <bence.hornak@gmail.com>
2023-03-06 00:30:04 +0100

    Remove unneeded setUp function

--- test/tree_test.dart
+++ test/tree_test.dart
@@ -4,41 +4,38 @@ import 'package:test/test.dart';
 
 void main() {
   group('Game tree tests', () {
-    GameWithVariations? game;
-    setUp(() {
-      game = GameWithVariations(
-          // Depth: 1st half move
-          [
-            // d4
-            GameNode(
-                AnnotatedMove(Color.WHITE, Chess.SQUARES['d2'],
-                    Chess.SQUARES['d4'], 0, PieceType.PAWN, null, null),
-                []),
-            // e4
-            GameNode(
-                AnnotatedMove(Color.WHITE, Chess.SQUARES['e2'],
-                    Chess.SQUARES['e4'], 0, PieceType.PAWN, null, null),
-                // Depth: 2nd half move
-                [
-                  // e5
-                  GameNode(
-                      AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
-                          Chess.SQUARES['e5'], 0, PieceType.PAWN, null, null),
-                      []),
-                  // e6
-                  GameNode(
-                      AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
-                          Chess.SQUARES['e6'], 0, PieceType.PAWN, null, null),
-                      [])
-                ])
-          ]);
-    });
+    GameWithVariations game = GameWithVariations(
+        // Depth: 1st half move
+        [
+          // d4
+          GameNode(
+              AnnotatedMove(Color.WHITE, Chess.SQUARES['d2'],
+                  Chess.SQUARES['d4'], 0, PieceType.PAWN, null, null),
+              []),
+          // e4
+          GameNode(
+              AnnotatedMove(Color.WHITE, Chess.SQUARES['e2'],
+                  Chess.SQUARES['e4'], 0, PieceType.PAWN, null, null),
+              // Depth: 2nd half move
+              [
+                // e5
+                GameNode(
+                    AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
+                        Chess.SQUARES['e5'], 0, PieceType.PAWN, null, null),
+                    []),
+                // e6
+                GameNode(
+                    AnnotatedMove(Color.BLACK, Chess.SQUARES['e7'],
+                        Chess.SQUARES['e6'], 0, PieceType.PAWN, null, null),
+                    [])
+              ])
+        ]);
     test('traverse() lastMove argument is correct', () {
       // This object is only used to accessed the move_to_san method (which should be static IMO)
       final chess = Chess();
 
       List<String> actualPath = [];
-      game!.traverse(
+      game.traverse(
           (Chess board, AnnotatedMove lastMove, List<AnnotatedMove> nextMoves) {
         actualPath.add(chess.move_to_san(lastMove));
       });
@@ -55,7 +52,7 @@ void main() {
       final chess = Chess();
 
       List<List<String>> actualPath = [];
-      game!.traverse(
+      game.traverse(
           (Chess board, AnnotatedMove lastMove, List<AnnotatedMove> nextMoves) {
         actualPath
             .add(nextMoves.map((move) => chess.move_to_san(move)).toList());
@@ -73,7 +70,7 @@ void main() {
       final chess = Chess();
 
       List<String> actualPath = [];
-      game!.traverse(
+      game.traverse(
           (Chess board, AnnotatedMove lastMove, List<AnnotatedMove> nextMoves) {
         actualPath.add(board.ascii);
       });
