6ba066c70a1afa8a86663bd7e73dfcb9dfe44917
Bence Hornák <bence.hornak@gmail.com>
2023-03-05 23:16:14 +0100

    Create test for traverse() nextMoves arg

--- test/tree_test.dart
+++ test/tree_test.dart
@@ -50,5 +50,23 @@ void main() {
       ];
       expect(actualPath, expectedDfsPath);
     });
+    test('traverse() nextMoves argument is correct', () {
+      // This object is only used to accessed the move_to_san method (which should be static IMO)
+      final chess = Chess();
+
+      List<List<String>> actualPath = [];
+      game!.traverse(
+          (Chess board, AnnotatedMove lastMove, List<AnnotatedMove> nextMoves) {
+        actualPath
+            .add(nextMoves.map((move) => chess.move_to_san(move)).toList());
+      });
+      List<List<String>> expectedDfsPath = [
+        [], // d4
+        ['e5', 'e6'], // e4
+        [], // e5
+        [], // e6
+      ];
+      expect(actualPath, expectedDfsPath);
+    });
   });
 }
