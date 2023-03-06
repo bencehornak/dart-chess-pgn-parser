b2437f3a5f14f44942f780b8da55681af5b3707f
Bence Hornák <bence.hornak@gmail.com>
2023-03-06 23:20:22 +0100

    Simplify test

--- test/pgn_reader_test.dart
+++ test/pgn_reader_test.dart
@@ -1,80 +1,20 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:chess/chess.dart';
 import 'package:chess_pgn_parser/chess_pgn_parser.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final expectedGameTree = [
-    GameWithVariations(
-        // Depth: 1st half move
-        [
-          // d4
-          GameNode(
-              AnnotatedMove(Color.WHITE, Chess.SQUARES['d2'],
-                  Chess.SQUARES['d4'], 0, PieceType.PAWN, null, null, 'd4'),
-              []),
-          // e4
-          GameNode(
-              AnnotatedMove(Color.WHITE, Chess.SQUARES['e2'],
-                  Chess.SQUARES['e4'], 0, PieceType.PAWN, null, null, 'e4'),
-              // Depth: 2nd half move
-              [
-                // e5
-                GameNode(
-                    AnnotatedMove(
-                        Color.BLACK,
-                        Chess.SQUARES['e7'],
-                        Chess.SQUARES['e5'],
-                        0,
-                        PieceType.PAWN,
-                        null,
-                        null,
-                        'e5'),
-                    [
-                      // Nc3
-                      GameNode(
-                          AnnotatedMove(
-                              Color.WHITE,
-                              Chess.SQUARES['b1'],
-                              Chess.SQUARES['c3'],
-                              0,
-                              PieceType.KNIGHT,
-                              null,
-                              null,
-                              'Nc3'),
-                          [
-                            // Nf6
-                            GameNode(
-                                AnnotatedMove(
-                                    Color.BLACK,
-                                    Chess.SQUARES['g8'],
-                                    Chess.SQUARES['f6'],
-                                    0,
-                                    PieceType.KNIGHT,
-                                    null,
-                                    null,
-                                    'Nf6'),
-                                []),
-                          ]),
-                    ]),
-                // e6
-                GameNode(
-                    AnnotatedMove(
-                        Color.BLACK,
-                        Chess.SQUARES['e7'],
-                        Chess.SQUARES['e6'],
-                        0,
-                        PieceType.PAWN,
-                        null,
-                        null,
-                        'e6'),
-                    [])
-              ])
-        ])
-  ];
+  final String expectedGameTree = '''
+[GameWithVariations(
+  1. d4
+  1. e4
+    1... e5
+      2. Nc3
+        2... Nf6
+    1... e6
+)]''';
 
   StreamSubscription<LogRecord>? loggerSubscription;
 
@@ -96,7 +36,7 @@ void main() {
 
       final tree = reader.parse();
 
-      expect(tree, expectedGameTree);
+      expect(tree.toString(), expectedGameTree);
     });
   });
 }
