e7114169623fe5334f8aee63d73883f1e9f7c905
Bence Hornák <bence.hornak@gmail.com>
2023-03-10 01:07:00 +0100

    Remove groups from test

--- test/pgn_reader_test.dart
+++ test/pgn_reader_test.dart
@@ -30,52 +30,44 @@ void main() {
     loggerSubscription!.cancel();
   });
 
-  group('simple', () {
-    test('fromString().parse() returns the tree', () async {
-      String input = await File('test/resources/pgn/simple.pgn').readAsString();
-      final reader = PgnReader.fromString(input);
+  test('simple.pgn - fromString().parse() returns the tree', () async {
+    String input = await File('test/resources/pgn/simple.pgn').readAsString();
+    final reader = PgnReader.fromString(input);
 
-      final tree = reader.parse();
+    final tree = reader.parse();
 
-      expect(tree.toString(), expectedGameTree);
-    });
+    expect(tree.toString(), expectedGameTree);
   });
-  group('advanced', () {
-    test('fromString().parse() returns the tree', () async {
-      String input =
-          await File('test/resources/pgn/advanced.pgn').readAsString();
-      final reader = PgnReader.fromString(input);
+  test('advanced.pgn - fromString().parse() returns the tree', () async {
+    String input = await File('test/resources/pgn/advanced.pgn').readAsString();
+    final reader = PgnReader.fromString(input);
 
-      List<GameWithVariations>? database;
-      expect(() => database = reader.parse(), returnsNormally);
-      expect(() => database!.toString(), returnsNormally);
-    });
+    List<GameWithVariations>? database;
+    expect(() => database = reader.parse(), returnsNormally);
+    expect(() => database!.toString(), returnsNormally);
   });
-  group('invalid input', () {
-    test('fromString().parse() throws PgnReaderException', () async {
-      String input =
-          await File('test/resources/pgn/invalid.pgn').readAsString();
-      final reader = PgnReader.fromString(input);
+  test('invalid.pgn fromString().parse() throws PgnReaderException', () async {
+    String input = await File('test/resources/pgn/invalid.pgn').readAsString();
+    final reader = PgnReader.fromString(input);
 
-      expect(
-          () => reader.parse(),
-          throwsA(isA<PgnReaderException>()
-              .having(
-                  (error) => error.ctx.start,
-                  'ctx.start',
-                  isA<Token>()
-                      // start Token
-                      .having((token) => token.line, 'line', 1)
-                      .having((token) => token.charPositionInLine,
-                          'charPositionInLine', 0))
-              .having(
-                  (error) => error.ctx.stop,
-                  'ctx.stop',
-                  isA<Token>()
-                      // stop Token
-                      .having((token) => token.line, "line", 1)
-                      .having((token) => token.charPositionInLine,
-                          "charPositionInLine", 0))));
-    });
+    expect(
+        () => reader.parse(),
+        throwsA(isA<PgnReaderException>()
+            .having(
+                (error) => error.ctx.start,
+                'ctx.start',
+                isA<Token>()
+                    // start Token
+                    .having((token) => token.line, 'line', 1)
+                    .having((token) => token.charPositionInLine,
+                        'charPositionInLine', 0))
+            .having(
+                (error) => error.ctx.stop,
+                'ctx.stop',
+                isA<Token>()
+                    // stop Token
+                    .having((token) => token.line, "line", 1)
+                    .having((token) => token.charPositionInLine,
+                        "charPositionInLine", 0))));
   });
 }
