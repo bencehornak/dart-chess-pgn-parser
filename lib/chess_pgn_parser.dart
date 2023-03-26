76f4ece5339b087df835de081f55ae32355e0855
Bence Hornák <bence.hornak@gmail.com>
2023-03-26 17:46:01 +0200

    Harmonize filenames

--- lib/chess_pgn_parser.dart
+++ lib/chess_pgn_parser.dart
@@ -4,4 +4,4 @@ export 'src/annotated_move.dart';
 export 'src/tree.dart';
 export 'src/chess_half_move_tree.dart';
 export 'src/pgn_reader.dart';
-export 'src/linear_chess_move_sequences.dart';
+export 'src/linear_move_sequence_tree.dart';
